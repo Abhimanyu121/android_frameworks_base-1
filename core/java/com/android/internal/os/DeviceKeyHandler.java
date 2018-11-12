f440717fd0903ccd47f5c928b757e90f3a5c8b68
Vjs Pranav <pranavasri@live.in>
2018-11-12 16:05:33 +0530

    Update DeviceKeyHandler.java

--- core/java/com/android/internal/os/DeviceKeyHandler.java
+++ core/java/com/android/internal/os/DeviceKeyHandler.java
@@ -11,8 +11,6 @@
 
 package com.android.internal.os;
 
-import android.content.Intent;
-import android.hardware.SensorEvent;
 import android.view.KeyEvent;
 
 public interface DeviceKeyHandler {
@@ -25,55 +23,4 @@ public interface DeviceKeyHandler {
      * @return If the event is consume
      */
     public KeyEvent handleKeyEvent(KeyEvent event);
-
-    /**
-     * Invoked when an unknown key was detected by the system,
-     * this should NOT handle the key just return if it WOULD be handled
-     *
-     * @param event The key event to be handled
-     * @return If the event will be consumed
-     */
-    public boolean canHandleKeyEvent(KeyEvent event);
-
-    /**
-     * Special key event that should be treated as
-     * a camera launch event
-     *
-     * @param event The key event to be handled
-     * @return If the event is a camera launch event
-     */
-    public boolean isCameraLaunchEvent(KeyEvent event);
-
-    /**
-     * Special key event that should be treated as
-     * a wake event
-     *
-     * @param event The key event to be handled
-     * @return If the event is a wake event
-     */
-    public boolean isWakeEvent(KeyEvent event);
-
-    /**
-     * Return false if this event should be ignored
-     *
-     * @param event The key event to be handled
-     * @return If the event should be ignored
-     */
-    public boolean isDisabledKeyEvent(KeyEvent event);
-
-    /**
-     * Return an Intent that should be launched for that KeyEvent
-     *
-     * @param event The key event to be handled
-     * @return an Intent or null
-     */
-    public Intent isActivityLaunchEvent(KeyEvent event);
-
-    default public String getCustomProxiSensor() {
-        return null;
-    }
-
-    default public boolean getCustomProxiIsNear(SensorEvent event) {
-        return false;
-    }
 }
